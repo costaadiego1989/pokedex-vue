774d39491e6c39b49404e029b571096bd79997ec
Diego <anovaterradecristo@gmail.com>
2023-02-27 16:54:28 -0300

    feat: realizado requisição na pokeapi com axios.

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,24 @@
 </template>
 
 <script>
+import axios from "axios"
+
 
 export default {
   name: 'App',
+  data() {
+    pokemons: [];
+  },
   components: {
+  },
+  created: async function() {
+    try {
+      const req = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0");
+      this.pokemons = req.data.results;
+      console.log("Pokemons", this.pokemons);
+    } catch (error) {
+      console.error("error", error);
+    }
   }
 }
 </script>
---
+++ src/components/PokeList.vue
@@ -0,0 +1,16 @@
+<template>
+</template>
+
+<script>
+
+export default {
+  name: 'PokeList',
+  data() {
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+</style>
